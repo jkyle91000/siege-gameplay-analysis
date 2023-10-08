6a767535fff9e303678eebf14829b220635a78b4
jkyle91000 <130641662+jkyle91000@users.noreply.github.com>
2023-10-08 02:37:43 -0500

    Add files via upload

--- everything.py
+++ everything.py
@@ -117,5 +117,67 @@ squads_table.to_excel("C:\\Users\\aethe\OneDrive\\Desktop\\stats\\Ranked 2 KD by
 
 #%%
 
-for i in data_filled:
-   break 
+squad_map_table = pd.DataFrame(columns = ["Map",
+                                          "Squad Size",
+                                          "K/D",
+                                          "KA/D"])
+
+#%%
+
+def kill_death_map_squad(map_name, squad_size):
+    
+    global kd, kda
+    
+    data_filtered = data_filled[data_filled["Map"] == map_name]
+        
+    kills = data_filled.loc[data_filled["Squad Size"] == squad_size, "Kills"].sum()
+    deaths = data_filled.loc[data_filled["Squad Size"] == squad_size, "Deaths"].sum()
+    assists = data_filled.loc[data_filled["Squad Size"] == squad_size, "Assists"].sum() * 1/3
+    
+    if deaths == 0:
+        kd = kills
+        kda = kills + assists
+    else:
+        kd = kills/deaths
+        kda = (kills + assists)/deaths
+
+    return map_name, squad_size, kd, kda
+    
+#%%
+
+kill_death_map_squad("Skyscraper", 5)
+
+#%%
+
+maps_for_table = []
+squad_sizes_for_table = []
+kd_ratios_for_table = []
+kd_assists_for_table = []
+
+#%%
+
+for i in maps:
+    for j in sizes:
+        kill_death_map_squad(i, j)
+        maps_for_table.append(i)
+        squad_sizes_for_table.append(j)
+        kd_ratios_for_table.append(kd)
+        kd_assists_for_table.append(kda)
+
+
+#%%
+
+smt = squad_map_table
+
+smt["Map"] = maps_for_table
+smt["Squad Size"] = squad_sizes_for_table
+smt["K/D"] = kd_ratios_for_table
+smt["KA/D"] = kd_assists_for_table
+
+#%%
+
+smt.to_excel("C:\\Users\\aethe\\OneDrive\\Desktop\\stats\\kd by map and squad size.xlsx")
+
+#%%
+
+
