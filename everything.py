2b3f901ebeeb0efc77a3c6ed751a00b3aab8b4de
jkyle91000 <130641662+jkyle91000@users.noreply.github.com>
2023-10-08 19:05:24 -0500

    Win/Loss and Big Fucking Table

--- everything.py
+++ everything.py
@@ -130,9 +130,9 @@ def kill_death_map_squad(map_name, squad_size):
     
     data_filtered = data_filled[data_filled["Map"] == map_name]
         
-    kills = data_filled.loc[data_filled["Squad Size"] == squad_size, "Kills"].sum()
-    deaths = data_filled.loc[data_filled["Squad Size"] == squad_size, "Deaths"].sum()
-    assists = data_filled.loc[data_filled["Squad Size"] == squad_size, "Assists"].sum() * 1/3
+    kills = data_filtered.loc[data_filtered["Squad Size"] == squad_size, "Kills"].sum()
+    deaths = data_filtered.loc[data_filtered["Squad Size"] == squad_size, "Deaths"].sum()
+    assists = data_filtered.loc[data_filtered["Squad Size"] == squad_size, "Assists"].sum() * 1/3
     
     if deaths == 0:
         kd = kills
@@ -180,4 +180,91 @@ smt.to_excel("C:\\Users\\aethe\\OneDrive\\Desktop\\stats\\kd by map and squad si
 
 #%%
 
+levels = []
+squad_sizes = []
+ratios = []
 
+def win_loss_map_squad(map_name, squad_size):
+    
+    global wl
+    
+    data_filtered = data_filled[data_filled["Map"] == map_name]
+    
+    wins = len(data_filtered[(data_filtered["Squad Size"] == squad_size) & (data_filtered["Outcome"] == "win")])
+    losses = len(data_filtered[(data_filtered["Squad Size"] == squad_size) & (data_filtered["Outcome"] == "loss")])
+    
+    if losses == 0:
+        wl = wins
+    else:
+        wl = wins/losses
+        
+    return wl
+        
+#%%
+
+for i in maps:
+    for j in sizes:
+        win_loss_map_squad(i, j)
+        levels.append(i)
+        squad_sizes.append(j)
+        ratios.append(wl)
+
+#%%
+
+win_loss_table = pd.DataFrame(columns = ["Map", "Squad Size", "W/L"])
+
+#%%
+
+win_loss_table["Map"] = levels
+win_loss_table["Squad Size"] = squad_sizes
+win_loss_table["W/L"] = ratios
+
+#%%
+
+win_loss_table.to_excel("C:\\Users\\aethe\\OneDrive\\Desktop\\stats\\win_loss_table.xlsx")
+
+#%%
+
+def all_the_damn_data(level_name, squad_size):
+
+    win_loss_map_squad(level_name, squad_size)
+    kill_death_map_squad(level_name, squad_size)
+    
+#%%
+
+levels = []
+squads = []
+kd_ratios = []
+kda_ratios = []
+wl_ratios = []
+
+for i in maps:
+    for j in sizes:
+        all_the_damn_data(i, j)
+        levels.append(i)
+        squads.append(j)
+        kd_ratios.append(kd)
+        kda_ratios.append(kda)
+        wl_ratios.append(wl)
+        
+#%%
+
+big_fucking_table = pd.DataFrame(columns = ["Map",
+                                            "Squad Size",
+                                            "K/D",
+                                            "KA/D",
+                                            "W/L"])
+
+#%%
+
+bft = big_fucking_table
+
+bft["Map"] = levels
+bft["Squad Size"] = squads
+bft["K/D"] = kd_ratios
+bft["KA/D"] = kda_ratios
+bft["W/L"] = wl_ratios
+
+#%%
+
+bft.to_excel("C:\\Users\\aethe\\OneDrive\\Desktop\\stats\\big_fucking_table.xlsx")
