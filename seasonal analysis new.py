b85ec79e53f741962d126e51d9119fab5dc2f296
jkyle91000 <130641662+jkyle91000@users.noreply.github.com>
2023-10-26 03:40:00 -0500

    Add files via upload

--- seasonal analysis new.py
+++ seasonal analysis new.py
@@ -150,43 +150,6 @@ def win_loss_squad(squad_size):
 
 #%%
 
-def win_loss_comparator():
-    
-    global season_wl_level_table, season_wl_squad_table, lifetime_wl_level_table, lifetime_wl_squad_table
-    
-    season_levels = ["Average Season W/L"]
-    season_level_wl_ratios = [avg_season_wl]
-    lifetime_levels = ["Lifetime W/L"]
-    lifetime_level_wl_ratios = [avg_lifetime_wl]
-    
-    season_squads = ["Average Season W/L"]
-    season_squad_wl_ratios = [avg_season_wl]
-    lifetime_squads = ["Lifetime W/L"]
-    lifetime_squad_wl_ratios = [avg_lifetime_wl]
-    
-    for level in levels:
-        win_loss_level(level)
-        season_levels.append(level)
-        season_level_wl_ratios.append(season_level_wl)
-        lifetime_levels.append(level)
-        lifetime_level_wl_ratios.append(lifetime_level_wl)
-        
-    for size in sizes:
-        win_loss_squad(size)
-        season_squads.append(size)
-        season_squad_wl_ratios.append(season_squad_wl)
-        lifetime_squads.append(size)
-        lifetime_squad_wl_ratios.append(lifetime_squad_wl)
-    
-    season_wl_level_table = pd.DataFrame({"Map": season_levels, "Season W/L": season_level_wl_ratios, })
-    season_wl_squad_table = pd.DataFrame({"Squad Size": season_squads, "W/L": season_squad_wl_ratios})
-    lifetime_wl_level_table = pd.DataFrame({"Map": lifetime_levels, "W/L": lifetime_level_wl_ratios})
-    lifetime_wl_squad_table = pd.DataFrame({"Squad Size": lifetime_squads, "W/L": lifetime_squad_wl_ratios})
-    
-    return season_wl_level_table, season_wl_squad_table, lifetime_wl_level_table, lifetime_wl_squad_table
-
-#%%
-
 def kill_death_level(level_name):
     
     global season_level_kd, season_level_kda, lifetime_level_kd, lifetime_level_kda
@@ -250,54 +213,291 @@ def kill_death_squad(squad_size):
     return season_squad_kd, season_squad_kda, lifetime_squad_kd, lifetime_squad_kda
 
 #%%
-def kill_death_comparator():
+
+def wl_comparator():
     
-    global season_kd_level_table, season_kd_squad_table, lifetime_kd_level_table, lifetime_kd_squad_table
+    global wl_levels_table, wl_squads_table
     
-    season_levels = ["Average Season K/D"]
-    season_level_kd_ratios = [avg_season_kd]
-    season_level_kda_ratios = [avg_season_kda]
-    lifetime_levels = ["Lifetime K/D"]
-    lifetime_level_kd_ratios = [avg_lifetime_kd]
-    lifetime_level_kda_ratios = [avg_lifetime_kda]
+    wl_levels = ["Average W/L"]
+    wl_ratios = [avg_season_wl]
+    lifetime_wl_ratios = [avg_lifetime_wl]
     
-    season_squads = ["Average Season K/D"]
-    season_squad_kd_ratios = [avg_season_kd]
-    season_squad_kda_ratios = [avg_season_kda]
-    lifetime_squads = ["Lifetime K/D"]
-    lifetime_squad_kd_ratios = [avg_lifetime_kd]
-    lifetime_squad_kda_ratios = [avg_lifetime_kda]
+    wl_squads = ["Average W/l"]
+    wl_ratios_squad = [avg_season_wl]
+    lifetime_wl_ratios_squad = [avg_lifetime_wl]
+    
+    for level in levels:
+        win_loss_level(level)
+        wl_levels.append(level)
+        wl_ratios.append(season_level_wl)
+        lifetime_wl_ratios.append(lifetime_level_wl)
+    
+    for size in sizes:
+        win_loss_squad(size)
+        wl_squads.append(size)
+        wl_ratios_squad.append(season_squad_wl)
+        lifetime_wl_ratios_squad.append(lifetime_squad_wl)
+        
+    wl_levels_table = pd.DataFrame({"Map": wl_levels, "Season W/L": wl_ratios, "Lifetime W/L": lifetime_wl_ratios})
+    wl_squads_table = pd.DataFrame({"Squad Size": wl_squads, "Season W/L": wl_ratios_squad, "Lifetime W/L": lifetime_wl_ratios_squad})
+    
+    return wl_levels_table, wl_squads_table
+
+def kd_comparator():
+    
+    global kd_levels_table, kd_squads_table
+    
+    kd_levels = ["Average K/D"]
+    kd_ratios = [avg_season_kd]
+    kda_ratios = [avg_season_kda]
+    lifetime_kd_ratios = [avg_lifetime_kd]
+    lifetime_kda_ratios = [avg_lifetime_kda]
+    
+    kd_squads = ["Average K/D"]
+    kd_ratios_squad = [avg_season_kd]
+    kda_ratios_squad = [avg_season_kda]
+    lifetime_kd_ratios_squad = [avg_lifetime_kd]
+    lifetime_kda_ratios_squad = [avg_lifetime_kda]
     
     for level in levels:
         kill_death_level(level)
-        season_levels.append(level)
-        season_level_kd_ratios.append(season_level_kd)
-        season_level_kda_ratios.append(season_level_kda)
-        lifetime_levels.append(level)
-        lifetime_level_kd_ratios.append(lifetime_level_kd)
-        lifetime_level_kda_ratios.append(lifetime_level_kda)
+        kd_levels.append(level)
+        kd_ratios.append(season_level_kd)
+        kda_ratios.append(season_level_kda)
+        lifetime_kd_ratios.append(lifetime_level_kd)
+        lifetime_kda_ratios.append(lifetime_level_kda)
         
     for size in sizes:
         kill_death_squad(size)
-        season_squads.append(size)
-        season_squad_kd_ratios.append(season_squad_kd)
-        season_squad_kda_ratios.append(season_squad_kda)
-        lifetime_squads.append(size)
-        lifetime_squad_kd_ratios.append(lifetime_squad_kd)
-        lifetime_squad_kda_ratios.append(lifetime_squad_kda)
+        kd_squads.append(size)
+        kd_ratios_squad.append(season_squad_kd)
+        kda_ratios_squad.append(season_squad_kda)
+        lifetime_kd_ratios_squad.append(lifetime_squad_kd)
+        lifetime_kda_ratios_squad.append(lifetime_squad_kda)
+        
+    kd_levels_table = pd.DataFrame({"Map": kd_levels,
+                                    "Season K/D": kd_ratios,
+                                    "Season KA/D": kda_ratios,
+                                    "Lifetime K/D": lifetime_kd_ratios,
+                                    "Lifetime KA/D": lifetime_kda_ratios})
+    kd_squads_table = pd.DataFrame({"Squad Size": kd_squads,
+                                    "Season K/D": kd_ratios_squad,
+                                    "Season KA/D": kda_ratios_squad,
+                                    "Lifetime K/D": lifetime_kd_ratios_squad,
+                                    "Lifetime KA/D": lifetime_kda_ratios_squad})
     
-    season_kd_level_table = pd.DataFrame({"Map": season_levels, "K/D": season_level_kd_ratios, "KA/D": season_level_kda_ratios})
-    season_kd_squad_table = pd.DataFrame({"Squad Size": season_squads, "K/D": season_squad_kd_ratios, "KA/D": season_squad_kda_ratios})
-    lifetime_kd_level_table = pd.DataFrame({"Map": lifetime_levels, "K/D": lifetime_level_kd_ratios, "KA/D": lifetime_level_kda_ratios})
-    lifetime_kd_squad_table = pd.DataFrame({"Squad Size": lifetime_squads, "K/D": lifetime_squad_kd_ratios, "KA/D": lifetime_squad_kda_ratios})
-    
-    return season_kd_level_table, season_kd_squad_table, lifetime_kd_level_table, lifetime_kd_squad_table
+    return kd_levels_table, kd_squads_table
 
 #%%
 
-win_loss_comparator()
-kill_death_comparator()
+def get_scores_levels(level_name):
+    
+    global season_avg_score, lifetime_avg_score
+    
+    season_filtered = season_scores[season_scores["Map"] == level_name]
+    lifetime_filtered = lifetime_scores[lifetime_scores["Map"] == level_name]
+    season_count = len(season_filtered)
+    lifetime_count = len(lifetime_filtered)
+    
+    season_total_score = season_filtered["Adjusted Score"].sum()
+    lifetime_total_score = lifetime_filtered["Adjusted Score"].sum()
+    
+    if season_count == 0:
+        season_avg_score = 0
+    else:
+        season_avg_score = season_total_score/season_count
+    
+    if lifetime_count == 0:
+        lifetime_avg_score = 0
+    else:
+        lifetime_avg_score = lifetime_total_score/lifetime_count
+    
+    return level_name, season_avg_score, lifetime_avg_score
+
+def get_scores_squads(squad_size):
+    
+    global season_avg_score, lifetime_avg_score
+    
+    season_scores_filtered = season_scores[season_scores["Squad Size"] == squad_size]
+    lifetime_scores_filtered = lifetime_scores[lifetime_scores["Squad Size"] == squad_size]
+    season_count = len(season_scores_filtered)
+    lifetime_count = len(lifetime_scores_filtered)
+    
+    season_total_score = season_scores_filtered["Adjusted Score"].sum()
+    lifetime_total_score = lifetime_scores_filtered["Adjusted Score"].sum()
+    
+    if season_count == 0:
+        season_avg_score = 0
+    else:
+        season_avg_score = season_total_score/season_count
+    
+    if lifetime_count == 0:
+        lifetime_avg_score = 0
+    else:
+        lifetime_avg_score = lifetime_total_score/season_count
+    
+    return squad_size, season_avg_score, lifetime_avg_score
 
 #%%
 
+def score_comparator():
+    
+    global score_levels_table, score_squads_table
+    
+    score_levels = ["Average Score"]
+    season_level_scores = [avg_season_score]
+    lifetime_level_scores = [avg_lifetime_score]
+    
+    score_squads = ["Average Score"]
+    season_squad_scores = [avg_season_score]
+    lifetime_squad_scores = [avg_lifetime_score]
+    
+    for level in levels:
+        get_scores_levels(level)
+        score_levels.append(level)
+        season_level_scores.append(season_avg_score)
+        lifetime_level_scores.append(lifetime_avg_score)
+        
+    for size in sizes:
+        get_scores_squads(size)
+        score_squads.append(size)
+        season_squad_scores.append(season_avg_score)
+        lifetime_squad_scores.append(lifetime_avg_score)
+        
+    score_levels_table = pd.DataFrame({"Map": score_levels,
+                                       "Season Score": season_level_scores,
+                                       "Lifetime Score": lifetime_level_scores})
+    score_squads_table = pd.DataFrame({"Squad Size": score_squads,
+                                       "Season Score": season_squad_scores,
+                                       "Lifetime Score": lifetime_squad_scores})
+    
+    return score_levels_table, score_squads_table
+
+
+#%%
+
+wl_comparator()
+kd_comparator()
+score_comparator()
+
+#%%
+
+def get_scores():
+    
+    global josh_all_levels_avg, asa_all_levels_avg, ed_all_levels_avg, luke_all_levels_avg
+    
+    josh_scores = season.dropna(subset = ["Josh Score"])
+    josh_scores.fillna(0)
+    josh_scores["Adjusted Score"] = josh_scores.loc[josh_scores["Outcome"] == "win", "Josh Score"] - 2000
+    josh_scores["Adjusted Score"] = josh_scores["Adjusted Score"].fillna(josh_scores.loc[josh_scores["Outcome"] == "loss", "Josh Score"])
+    
+    josh_total = josh_scores["Adjusted Score"].sum()
+    josh_count = len(josh_scores)
+    josh_all_levels_avg = josh_total/josh_count
+    
+    asa_scores = season.dropna(subset = ["Asa Score"])
+    asa_scores.fillna(0)
+    asa_scores["Adjusted Score"] = asa_scores.loc[asa_scores["Outcome"] == "win", "Asa Score"] - 2000
+    asa_scores["Adjusted Score"] = asa_scores["Adjusted Score"].fillna(asa_scores.loc[asa_scores["Outcome"] == "loss", "Asa Score"])
+    
+    asa_total = asa_scores["Adjusted Score"].sum()
+    asa_count = len(asa_scores)
+    asa_all_levels_avg = asa_total/asa_count
+    
+    ed_scores = season.dropna(subset = ["Ed Score"])
+    ed_scores.fillna(0)
+    ed_scores["Adjusted Score"] = ed_scores.loc[ed_scores["Outcome"] == "win", "Ed Score"] - 2000
+    ed_scores["Adjusted Score"] = ed_scores["Adjusted Score"].fillna(ed_scores.loc[ed_scores["Outcome"] == "loss", "Ed Score"])
+    
+    ed_total = ed_scores["Adjusted Score"].sum()
+    ed_count = len(ed_scores)
+    ed_all_levels_avg = ed_total/ed_count
+    
+    luke_scores = season.dropna(subset = ["Luke Score"])
+    luke_scores.fillna(0)
+    luke_scores["Adjusted Score"] = luke_scores.loc[luke_scores["Outcome"] == "win", "Luke Score"] - 2000
+    luke_scores["Adjusted Score"] = luke_scores["Adjusted Score"].fillna(luke_scores.loc[luke_scores["Outcome"] == "loss", "Luke Score"])
+    
+    luke_total = luke_scores["Adjusted Score"].sum()
+    luke_count = len(luke_scores)
+    luke_all_levels_avg = luke_total/luke_count
+    
+    return josh_all_levels_avg, asa_all_levels_avg, ed_all_levels_avg, luke_all_levels_avg
+
+#%%
+
+def individual_performance_calculator(level_name):
+    
+    global josh_avg, asa_avg, ed_avg, luke_avg
+    
+    josh_filtered = josh_scores[josh_scores["Map"] == level_name]
+    asa_filtered = asa_scores[asa_scores["Map"] == level_name]
+    ed_filtered = ed_scores[ed_scores["Map"] == level_name]
+    luke_filtered = luke_scores[luke_scores["Map"] == level_name]
+    
+    josh_count = len(josh_filtered)
+    asa_count = len(asa_filtered)
+    ed_count = len(ed_filtered)
+    luke_count = len(luke_filtered)
+    
+    josh_total = josh_filtered["Adjusted Score"].sum()
+    asa_total = asa_filtered["Adjusted Score"].sum()
+    ed_total = ed_filtered["Adjusted Score"].sum()
+    luke_total = luke_filtered["Adjusted Score"].sum()
+    
+    if josh_count == 0:
+        josh_avg = 0
+    else:
+        josh_avg = josh_total/josh_count
+
+    if asa_count == 0:
+        asa_avg = 0
+    else:
+        asa_avg = asa_total/asa_count
+        
+    if ed_count == 0:
+        ed_avg = 0
+    else:
+        ed_avg = ed_total/ed_count
+        
+    if luke_count == 0:
+        luke_avg = 0
+    else:
+        luke_avg = luke_total/luke_count
+    
+    return level_name, josh_avg, asa_avg, ed_avg, luke_avg
+
+#%%
+
+def individual_perforamnce_constructor():
+    
+    global ip_table
+    
+    ip_score_levels = ["Average Score"]
+    ip_josh_scores = [josh_all_levels_avg]
+    ip_asa_scores = [asa_all_levels_avg]
+    ip_ed_scores = [ed_all_levels_avg]
+    ip_luke_scores = [luke_all_levels_avg]
+    
+    for level in levels:
+        individual_performance_calculator(level)
+        ip_score_levels.append(level)
+        ip_josh_scores.append(josh_avg)
+        ip_asa_scores.append(asa_avg)
+        ip_ed_scores.append(ed_avg)
+        ip_luke_scores.append(luke_avg)
+        
+    ip_table = pd.DataFrame({"Map": ip_score_levels,
+                             "Josh Score": ip_josh_scores,
+                             "Asa Score": ip_asa_scores,
+                             "Ed Score": ip_ed_scores,
+                             "Luke Score": ip_luke_scores})
+    
+    return ip_table
+
+#%%
+
+individual_perforamnce_constructor()
+
+#%%
 
